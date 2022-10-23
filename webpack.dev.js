b6cbb1dee06972e2e06b9c296bf65e43f7691e07
Saleh-387 <abdshaleh124@webmail.umm.ac.id>
2022-10-23 17:27:00 +0700

    Fix Bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     devServer: {
         static: path.resolve(__dirname, 'dist'),
         open: true,
-        port: 7024,
+        port: 7017,
         client: {
             overlay: {
                 errors: true,
